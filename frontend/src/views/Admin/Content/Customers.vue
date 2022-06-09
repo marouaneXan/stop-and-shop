655617db07f7e5afabc47e9f5c13f250a97cad12
marouane zahaoui <marouanezahaoui835@gmail.com>
2022-06-09 23:53:52 +0100

    still working

--- frontend/src/views/Admin/Content/Customers.vue
+++ frontend/src/views/Admin/Content/Customers.vue
@@ -9,79 +9,17 @@
     <div class="col-md-12 fs-5 mt-3 mb-2" style="padding:0px 50px;">
         Name of Customer
     </div>
-    <div class="d-flex" id="form" style="">
+    <div class="d-flex" id="form">
         <div class="mb-3" style="padding:0px 50px;">
             <input v-model="searchQuery" type="text" class="form-control" placeholder="Search" style="width:300px;">
         </div>
     </div>
     <div class="col mt-5" style="padding:0px 50px;">
-
-        <!-- Model To add new product -->
-        <button type="button" class="btn btn-info" data-bs-toggle="modal" data-bs-target="#addProduct">Add Product</button>
-        <div class="modal fade" id="addProduct" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalLabel">Add New Product</h5>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body">
-                        <form @submit="addProduct()">
-                            <div class="mb-1">
-                                <label for="name" class="form-label">Name</label>
-                                <input type="text" v-model="product.nom" class="form-control" id="name" required>
-                                <label for="Description" class="form-label">Description</label>
-                                <input type="text" v-model="product.description" class="form-control" id="Description" required>
-                                <label for="Price" class="form-label">Price</label>
-                                <input type="text" v-model="product.prix" class="form-control" id="Price" required>
-                                <!-- <label for="formFile" class="form-label">Image</label> -->
-                                <!-- <input type="file" ref="ProductImage" multiple class="form-control" @change="previewFiles" id="formFile" enctype='multipart/form-data'> -->
-                                <select class="form-select" v-model="product.id_category" aria-label="Default select example">
-                                    <option disabled selected>Select Category</option>
-                                    <option v-for="c in categories" :key="c.id_cat" :value="c.id_cat" required>{{c.nom_cat}}</option>
-                                </select>
-                                <label for="Quantity" class="form-label">Quantity</label>
-                                <input type="number" v-model="product.quantite" class="form-control" id="Quantity" required>
-                            </div>
-                        </form>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                        <button type="submit" @click="addProduct()" class="btn btn-primary" data-bs-dismiss="modal">Add</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <!-- Model To add new category -->
-        <button class="btn btn-secondary" data-bs-toggle="modal" data-bs-target="#addCategory" style="margin-left:8px">Add Category</button>
-        <div class="modal fade" id="addCategory" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalLabel">Add New Category</h5>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body">
-                        <form @click.prevent>
-                            <div class="mb-1">
-                                <label for="name" class="form-label">Name of Category</label>
-                                <input v-model="category.nom_cat" type="text" class="form-control" id="name" required>
-                            </div>
-                        </form>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                        <button @click="addCategory()" type="button" class="btn btn-primary" data-bs-dismiss="modal">Add</button>
-                    </div>
-                </div>
-            </div>
-        </div>
     </div>
     <div  class="row d-flex justify-content-center align-items-center mt-3">
         <div class="col-md-6" id="card">
             <div class="card text-dark bg-light mb-3">
-                <div class="card-header fw-bold">List of Products</div>
+                <div class="card-header fw-bold">List of Customers</div>
                 <div class="card-body">
                     <div class="table-responsive">
                         <table class="table table-striped">
