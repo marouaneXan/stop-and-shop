7b0aa1841eaba68e64365194ef20e8b69f366c55
marouane zahaoui <marouanezahaoui835@gmail.com>
2022-06-10 11:25:42 +0100

    add function updateOrder()

--- frontend/src/views/Admin/Content/Orders.vue
+++ frontend/src/views/Admin/Content/Orders.vue
@@ -3,8 +3,8 @@
 <SidebarComponent />
 <main class="mt-5 pt-3">
     <!--Message for delete products-->
-    <!-- <div v-if="Deletecustomer.success" class="alert alert-danger text-center">{{Deletecustomer.success}}</div>
-    <div v-if="Deletecustomer.error" class="alert alert-warning text-center">{{Deletecustomer.error}}</div> -->
+    <div v-if="Updateorder.success" class="alert alert-danger text-center">{{Updateorder.success}}</div>
+    <div v-if="Updateorder.error" class="alert alert-warning text-center">{{Updateorder.error}}</div>
 
     <div class="col-md-12 fs-5 mt-3 mb-2" style="padding:0px 50px;">
         Name of customer
@@ -54,25 +54,37 @@
                                     <td>{{o.qtte}}</td>
                                     <td>{{o.done_at}}</td>
                                     <td>
-                                      <span v-if="o.status=='In progress...'" class="badge rounded-pill bg-warning">In progress...</span>
-                                      <span v-if="o.status=='Delivery'" class="badge rounded-pill bg-info">Delivery</span>
-                                      <span v-if="o.status=='Confirmed'" class="badge rounded-pill bg-success">Confirmed</span>
+                                        <span v-if="o.status=='In progress...'" class="badge rounded-pill bg-warning">In progress...</span>
+                                        <span v-if="o.status=='Delivery'" class="badge rounded-pill bg-info">Delivery</span>
+                                        <span v-if="o.status=='Confirmed'" class="badge rounded-pill bg-success">Confirmed</span>
                                     </td>
                                     <td class="action_btn">
-                                        <button class="btn" @click="passingDataDelete(p)" data-bs-toggle="modal" data-bs-target="#delete"><i class="fa-solid fa-trash-can" style="color:red;"></i></button>
-                                        <div class="modal fade" id="delete" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                                            <div class="modal-dialog modal-dialog-centered">
+                                        <button @click="passingDataUpdate(o)" class="btn" data-bs-toggle="modal" data-bs-target="#updateProduct"><i class="fa-solid fa-pen-to-square"></i></button>
+                                        <!-- Model To update new product -->
+                                        <div class="modal fade" id="updateProduct" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                            <div class="modal-dialog">
                                                 <div class="modal-content">
                                                     <div class="modal-header">
-                                                        <h5 class="modal-title" id="exampleModalLabel">Delete Product</h5>
+                                                        <h5 class="modal-title" id="exampleModalLabel">Update Product</h5>
                                                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                                     </div>
                                                     <div class="modal-body">
-                                                        Are You Sure You want To Delete This Customer
+                                                        <form @click.prevent>
+                                                            <div class="mb-1">
+                                                                <!-- <label for="name" class="form-label">Status</label>
+                                                                <input type="text" v-model="order.status" class="form-control" id="name"> -->
+                                                                <select class="form-select" aria-label="Default select example">
+                                                                    <option disabled selected>Select Status</option>
+                                                                    <option value="In progress...">In progress...</option>
+                                                                    <option value="Delivery">Delivery</option>
+                                                                    <option value="Confirmed">Confirmed</option>
+                                                                </select>
+                                                            </div>
+                                                        </form>
                                                     </div>
                                                     <div class="modal-footer">
-                                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
-                                                        <!-- <button type="button" @click="DeleteCustomer()" class="btn btn-danger" data-bs-dismiss="modal">Yes</button> -->
+                                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                                        <button type="button" @click="UpdateOrder()" class="btn btn-primary" data-bs-dismiss="modal">Update</button>
                                                     </div>
                                                 </div>
                                             </div>
@@ -99,14 +111,15 @@ export default {
     data() {
         return {
             orders: [],
-            searchQuery:'',
+            searchQuery: '',
             order: {
-                id_pers:'',
+                id_pers: '',
+                status: ''
             },
-            // Deletecustomer: {
-            //     success: '',
-            //     error: ''
-            // },
+            Updateorder: {
+                success: '',
+                error: ''
+            }
         }
     },
     components: {
@@ -127,30 +140,38 @@ export default {
             }
         }
     },
-    methods:{
-      async fetchOrders() {
+    methods: {
+        async fetchOrders() {
             let res = await axios("http://stop-and-shop.com/Order");
             this.orders = res.data
         },
-        //passing id for model
-        // passingDataDelete(p) {
-        //     this.customer.id_pers = p.id_pers;
-        //     console.log(this.customer.id_pers)
-        // },
-        //delete customer
-        // async DeleteCustomer() {
-        //     let res = await axios.post("http://stop-and-shop.com/User/DeleteCustomers/" + this.customer.id_pers);
-        //     if (res.data.message == 'Customer Deleted Successfully') {
-        //         this.fetchOrders()
-        //         this.Deletecustomer.success = "Customer Deleted Successfully";
-        //     } else {
-        //         this.fetchCustomers()
-        //         this.Deletecustomer.error = "Error on updating customer";
-        //     }
-        // },
+        //passing data for model
+        passingDataUpdate(o) {
+            this.order.id_order = o.id_order;
+            this.order.status = o.status;
+        },
+        //delete product
+        async UpdateProduct() {
+            var form = new FormData();
+            form.append('nom', this.order.status);
+            let res = await axios({
+                method: "POST",
+                url: 'http://stop-and-shop.com/Order/updateOrder/' + this.order.id_order,
+                data: form,
+                headers: {
+                    "Content-Type": "multipart/form-data"
+                },
+            })
+            if (res.data.message == "Status Updated successfully") {
+                this.fetchOrders()
+                this.Updateorder.success = res.data.message;
+            } else {
+                this.fetchOrders()
+                this.Updateorder.error = "Error on Updating Status";
+            }
+        },
     },
-    
-        
+
 }
 </script>
 
