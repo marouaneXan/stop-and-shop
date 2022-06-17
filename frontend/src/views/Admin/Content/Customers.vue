e0efe91227461f3742c4210f2b42a141288b0b33
marouane zahaoui <marouanezahaoui835@gmail.com>
2022-06-17 11:07:54 +0100

    redirect user not login to page home when he wants to go to admin/customers

--- frontend/src/views/Admin/Content/Customers.vue
+++ frontend/src/views/Admin/Content/Customers.vue
@@ -109,6 +109,11 @@ export default {
                 val: 'home'
             });
         }
+        if (!client){
+            this.redirect({
+                val: 'home'
+            });
+        }
     },
     computed: {
         resultQuery() {
